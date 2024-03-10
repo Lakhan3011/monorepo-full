e6fa6f75b1e3263acbb29186edaa9f4bbea85e59
Lakhan lal <lakhan191136@dei.ac.in>
2024-03-10 16:54:48 +0530

    cp-1

--- apps/client-next/src/pages/index.tsx
+++ apps/client-next/src/pages/index.tsx
@@ -1,17 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import {Button} from "ui";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Button } from "ui";
+import { UserInputType } from "common";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <main className={`${styles.main} ${inter.className}`}>
-       <Button />
+        <Button />
+        <button
+          onClick={() => {
+            let userDetails: UserInputType = {
+              email: "",
+              password: "",
+            };
+          }}
+        ></button>
       </main>
     </>
-  )
+  );
 }
--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3002
-import { UserInput } from "common"
+const express = require("express");
+const app = express();
+const port = 3002;
+import { UserInput } from "common";
 
-app.get('/', (req: any, res: any) => {
+app.get("/", (req: any, res: any) => {
   let parsedUser = UserInput.safeParse(req.body);
   if (!parsedUser.success) {
-    res.send('Incorrect input for harkirat!')
+    res.send("Incorrect input for harkirat!");
     return;
   }
-  res.send('correct input!')
-})
+  res.send("correct input!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
